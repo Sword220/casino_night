cc4f42b3906a94a4fb2233997d1610e4d59ebdeb
primalfunk <31257920+primalfunk@users.noreply.github.com>
2018-05-12 20:34:55 -0600

    Added Wallet class with method add_to_wallet

--- menu.rb
+++ menu.rb
@@ -1,6 +1,16 @@
 require 'colorize'
 require_relative 'blackjack'
 
+class Wallet
+   def initialize
+       @player_wallet = 500
+   end
+  
+   def add_to_wallet(amount)
+       @player_wallet += amount
+   end
+end
+
 def menu
     puts "GAME MENU"
     puts "1) Black Jack"
@@ -15,6 +25,7 @@ end
 
 
 def selections   
+    money = Wallet.new
     choice = ""
     while choice != 4
         puts "Please choose a game to play:"
@@ -25,6 +36,7 @@ def selections
             @dealer.dealer_shuffle(@deck)
             @dealer.deal(@deck, 2, true)
             @dealer.question(@deck)
+            money.add_to_wallet(@blackjack_earnings)
         elsif choice == 2
             SlotMachine
         elsif choice == 3
